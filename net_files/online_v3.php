70b315e651b3180359ca854342f0eea51b33410f
jarvus <1151770629@qq.com>
2016-10-28 21:50:37 +0800

    删除不需要的文件

--- net_files/online_v3.php
+++
@@ -1,6 +0,0 @@
-var node=document.getElementById('cnzz_stat_icon_'+950900);
-if(node){
-    node.innerHTML="<a href=http://www.cnzz.com/stat/website.php?web_id=950900 target=_blank>站长统计</a><a href=http://www.cnzz.com/stat/website.php?web_id=950900 target=_blank> |  今日IP[936283] | 今日PV[31708509] | 昨日IP[1189520] |  昨日PV[40926678] | <a href=http://www.cnzz.com/stat/website.php?web_id=950900&method=online target=_blank>当前在线[42209]</a>";
-}else{
-    document.write("<a href=http://www.cnzz.com/stat/website.php?web_id=950900 target=_blank>站长统计</a><a href=http://www.cnzz.com/stat/website.php?web_id=950900 target=_blank> |  今日IP[936283] | 今日PV[31708509] | 昨日IP[1189520] |  昨日PV[40926678] | <a href=http://www.cnzz.com/stat/website.php?web_id=950900&method=online target=_blank>当前在线[42209]</a>");
-}
