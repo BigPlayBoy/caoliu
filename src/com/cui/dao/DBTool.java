26e122a9aaf0d1c082437c79424ac5977c502441
jarvus <1151770629@qq.com>
2016-10-31 10:38:59 +0800

    在intellij idea中commit

---
+++ src/com/cui/dao/DBTool.java
@@ -0,0 +1,87 @@
+package com.cui.dao;
+
+import com.cui.Utils.DBUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Created by jarvis on 2016/10/31.
+ * 数据库的增删改查
+ */
+public class DBTool {
+
+    private static Logger logger = LoggerFactory.getLogger("DBTool.class");
+
+
+    /**
+     * 查询数据库
+     *
+     * @param sql 待执行语句
+     * @return 查询结果集
+     */
+    private static ResultSet select(String sql) {
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = DBUtil.getConnection();
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(sql);
+            return resultSet;
+        } catch (SQLException e) {
+            logger.error("查询数据库出错" + e);
+            logger.error("查询数据库的语句为：" + sql);
+            return null;
+        } finally {
+            DBUtil.releaseDB(statement, connection);
+        }
+    }
+
+    /**
+     * 删除数据库某条信息
+     *
+     * @param sql 待执行语句
+     * @return 真/假
+     */
+    private static Boolean delete(String sql) {
+        return update(sql);
+    }
+
+    /**
+     * 插入数据库
+     *
+     * @param sql 待执行语句
+     * @return 真/假
+     */
+    private static Boolean insert(String sql) {
+        return update(sql);
+    }
+
+    /**
+     * 更新数据库，插入，删除，也是调用这个
+     *
+     * @param sql 待执行语句
+     * @return 返回是否成功
+     */
+    private static Boolean update(String sql) {
+        Connection connection = null;
+        Statement statement = null;
+        try {
+            connection = DBUtil.getConnection();
+            statement = connection.createStatement();
+            statement.executeUpdate(sql);
+            return true;
+        } catch (SQLException e) {
+            logger.error("更新数据库出错" + e);
+            logger.error("更新数据库的语句为：" + sql);
+            return false;
+        } finally {
+            DBUtil.releaseDB(statement, connection);
+        }
+    }
+}
